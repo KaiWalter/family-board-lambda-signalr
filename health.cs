de608dfddef67b8809800d8ab5a828306c30c7f5
Kai Walter <kai.walter@zeiss.com>
2019-04-14 12:54:10 +0200

    initial

---
+++ health.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace FamilyBoardInteractive
+{
+    public static class health
+    {
+        [FunctionName("health")]
+        public static async Task<IActionResult> Run(
+            [HttpTrigger(AuthorizationLevel.Function, "get", Route = null)] HttpRequest req,
+            ILogger log)
+        {
+            return (ActionResult)new OkObjectResult($"It's alive");
+        }
+    }
+}
