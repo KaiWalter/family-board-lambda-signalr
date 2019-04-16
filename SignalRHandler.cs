d114eaec7cc2490bef81dd0d61ab9e6285ebd6ab
Kai Walter <kai.walter@zeiss.com>
2019-04-16 17:35:58 +0000

    SignalR integrated

---
+++ SignalRHandler.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Extensions.Http;
+using Microsoft.Azure.WebJobs.Extensions.SignalRService;
+using System.Threading.Tasks;
+
+namespace FamilyBoardInteractive
+{
+    public static class SignalRHandler
+    {
+        private const string HUBNAME = "fb";
+
+        [FunctionName("negotiate")]
+        public static SignalRConnectionInfo Negotiate(
+            [HttpTrigger(AuthorizationLevel.Anonymous)]HttpRequest req,
+            [SignalRConnectionInfo(HubName = HUBNAME)]SignalRConnectionInfo connectionInfo)
+        {
+            return connectionInfo;
+        }
+
+        [FunctionName("SendMessage")]
+        public static Task SendMessage(
+            [HttpTrigger(AuthorizationLevel.Anonymous, "post")]object message,
+            [SignalR(HubName = HUBNAME)]IAsyncCollector<SignalRMessage> signalRMessages)
+        {
+            return signalRMessages.AddAsync(
+                new SignalRMessage
+                {
+                    Target = "newMessage",
+                    Arguments = new[] { message }
+                });
+        }
+    }
+}
