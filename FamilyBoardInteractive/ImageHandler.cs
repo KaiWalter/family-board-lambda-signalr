e5655d7f02884af0c5f3a56cee26796525dadcf0
Kai Walter <kai.walter@outlook.de>
2019-06-09 09:10:29 +0200

    split out storage handling

--- FamilyBoardInteractive/ImageHandler.cs
+++ FamilyBoardInteractive/ImageHandler.cs
@@ -53,46 +53,6 @@ namespace FamilyBoardInteractive
             return new OkResult();
         }
 
-        [FunctionName(nameof(QueuedConfiguartionCheck))]
-        [Singleton(Mode = SingletonMode.Listener)]
-        public static async Task QueuedConfiguartionCheck(
-                [QueueTrigger(Constants.QUEUEMESSAGECONFIGURUATION)]string queueMessage,
-                [Blob(Constants.BLOBPATHCONTAINER, FileAccess.ReadWrite)] CloudBlobContainer storageContainer,
-                ILogger log)
-        {
-            await CheckStorageConfiguration(storageContainer, log);
-        }
-
-        /// <summary>
-        /// Check the storage configuration
-        /// </summary>
-        /// <param name="storageContainer">reference to Blob Storage Container</param>
-        /// <returns></returns>
-        private static async Task CheckStorageConfiguration(CloudBlobContainer storageContainer, ILogger log)
-        {
-            if (await storageContainer.CreateIfNotExistsAsync())
-            {
-                log.LogInformation($"creating container {Constants.BLOBPATHCONTAINER}");
-                await storageContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Off });
-            }
-
-            string blobName = Constants.BLOBPATHIMAGESPLAYED.Replace(Constants.BLOBPATHCONTAINER + "/", "");
-            var blob = storageContainer.GetBlockBlobReference(blobName);
-
-            if (!await blob.ExistsAsync())
-            {
-                log.LogInformation($"creating blob {Constants.BLOBPATHIMAGESPLAYED}");
-
-                blob.Properties.ContentType = Constants.BLOBCONTENTTYPEIMAGESPLAYED;
-
-                var imagesPlayedStorage = new ImagesPlayedStorage { ImagesPlayed = new List<ImagePlayed>() };
-
-                string blobContent = JsonConvert.SerializeObject(imagesPlayedStorage);
-
-                await blob.UploadTextAsync(blobContent);
-            }
-        }
-
         [FunctionName(nameof(QueuedPushNextImage))]
         [Singleton(Mode = SingletonMode.Listener)]
         public static async Task QueuedPushNextImage(
---
+++ FamilyBoardInteractive/Storage.cs
@@ -0,0 +1,56 @@
+﻿using FamilyBoardInteractive.Models;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Extensions.Logging;
+using Microsoft.WindowsAzure.Storage.Blob;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace FamilyBoardInteractive
+{
+    public class Storage
+    {
+        [FunctionName(nameof(QueuedConfiguartionCheck))]
+        [Singleton(Mode = SingletonMode.Listener)]
+        public static async Task QueuedConfiguartionCheck(
+                [QueueTrigger(Constants.QUEUEMESSAGECONFIGURUATION)]string queueMessage,
+                [Blob(Constants.BLOBPATHCONTAINER, FileAccess.ReadWrite)] CloudBlobContainer storageContainer,
+                ILogger log)
+        {
+            await CheckStorageConfiguration(storageContainer, log);
+        }
+
+        /// <summary>
+        /// Check the storage configuration
+        /// </summary>
+        /// <param name="storageContainer">reference to Blob Storage Container</param>
+        /// <returns></returns>
+        private static async Task CheckStorageConfiguration(CloudBlobContainer storageContainer, ILogger log)
+        {
+            if (await storageContainer.CreateIfNotExistsAsync())
+            {
+                log.LogInformation($"creating container {Constants.BLOBPATHCONTAINER}");
+                await storageContainer.SetPermissionsAsync(new BlobContainerPermissions { PublicAccess = BlobContainerPublicAccessType.Off });
+            }
+
+            string blobName = Constants.BLOBPATHIMAGESPLAYED.Replace(Constants.BLOBPATHCONTAINER + "/", "");
+            var blob = storageContainer.GetBlockBlobReference(blobName);
+
+            if (!await blob.ExistsAsync())
+            {
+                log.LogInformation($"creating blob {Constants.BLOBPATHIMAGESPLAYED}");
+
+                blob.Properties.ContentType = Constants.BLOBCONTENTTYPEIMAGESPLAYED;
+
+                var imagesPlayedStorage = new ImagesPlayedStorage { ImagesPlayed = new List<ImagePlayed>() };
+
+                string blobContent = JsonConvert.SerializeObject(imagesPlayedStorage);
+
+                await blob.UploadTextAsync(blobContent);
+            }
+        }
+
+
+    }
+}
